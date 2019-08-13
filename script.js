b351e66ed57209dbf9ea40ae0db11e7a01d223e0
orangegrove_1955 <orangegrove_1955@hotmail.com>
2019-08-13 15:42:39 +1000

    Added loading section before show, added Skycons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import {apiKey} from './secrets.js';
+
 const infoSection = document.querySelector('#infoSection');
 const loadingSection = document.querySelector('#loadingSection');
 
@@ -7,6 +8,7 @@ window.addEventListener('load', () => {
     const temperatureDescription = document.querySelector('.temperature-description');
     const temperatureDegree = document.querySelector('.temperature-degree');
     const locationTimezone = document.querySelector('.location-timezone');
+    const weatherIcon = document.querySelector('.weather-icon');
 
     // Check if geolocation available before using
     if(navigator.geolocation){
@@ -15,38 +17,59 @@ window.addEventListener('load', () => {
             // Store latitutde and longitude values from Geolocation API
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            
-            // Need to use a proxy to allow CORS from localhost
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            // API call to Dark Sky API using secret key
-            const api = `${proxy}https://api.darksky.net/forecast/${apiKey}/${lat},${long}`;
-            fetch(api)
-                .then(response => {
-                    // Format data into json
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    // Use data to populate page
-                    const {temperature, summary} = data.currently;
-                    temperatureDegree.textContent = temperature.toFixed(0);
-                    temperatureDescription.textContent = summary;
-                    // Get just the city name for location
-                    const zone = data.timezone.split('/');
-                    locationTimezone.textContent = zone[1];
-                })
-                .then(() => {
-                    loadingSection.classList.add('hidden');
-                    infoSection.classList.remove('hidden');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        });
+            getWeather(lat, long);
+        },
+        error => {
+            alert('Sorry, your position could not be found, defaulting to New York');
+            lat = 40.730610;
+            long = -73.935242;
+            getWeather(lat, long);
+        },
+        {timeout: 10000}
+        );
+
     }
     else{
         // TODO: Fix to display error message when location not available
         const errorStatement = document.querySelector('#loadCaption');
         errorStatement.textContent = "Sorry, your browser does not currently support geolocation";
+    }
+
+    function getWeather(lat, long) {
+        // Need to use a proxy to allow CORS from localhost
+        const proxy = "https://cors-anywhere.herokuapp.com/";
+        // API call to Dark Sky API using secret key
+        const api = `${proxy}https://api.darksky.net/forecast/${apiKey}/${lat},${long}`;
+        fetch(api)
+            .then(response => {
+                // Format data into json
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                // Use data to populate page
+                const {temperature, summary, icon} = data.currently;
+                temperatureDegree.textContent = temperature.toFixed(0);
+                temperatureDescription.textContent = summary;
+                // Get just the city name for location
+                const zone = data.timezone.split('/');
+                locationTimezone.textContent = zone[1].replace(/_/g, ' ');
+                setIcons(icon, weatherIcon);
+            })
+            .then(() => {
+                loadingSection.classList.add('hidden');
+                infoSection.classList.remove('hidden');
+                infoSection.classList.add('flex');
+            })
+            .catch(err => {
+                alert('There was a problem getting the current weather, please try reloading the page');
+            });
+    }
+
+    function setIcons(icon, iconID) {
+         const skycons = new Skycons({color: "white"});
+         const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+         skycons.play();
+         return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
